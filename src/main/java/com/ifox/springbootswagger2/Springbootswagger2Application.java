b08cd5c2c171d54d3d26537c2cb61ebc7c8db3f6
danchede <1320030382@qq.com>
2018-06-21 20:06:48 +0800

    first

---
+++ src/main/java/com/ifox/springbootswagger2/Springbootswagger2Application.java
@@ -0,0 +1,12 @@
+package com.ifox.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootswagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootswagger2Application.class, args);
+	}
+}
